57d2863df796f71af80bae70f930fcb36c939f21
Annie Ullyot <50632137+akullyot@users.noreply.github.com>
2024-02-27 13:26:54 -0600

    removed testing and web vitals and imported and formatted all needed airport json data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+//Import in all required hooks and dependencies
+
+//Import in all required datasets
+import iataCodes from './Assets/Data/IATAcodes.js';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
